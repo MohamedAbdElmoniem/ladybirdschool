f858cb86e1c0ae091b130be823f8f870559afbe2
Mohamed-AbdElMoniem-AbdElMegeed <mohamed.abdelmoniem-abdelmegeed@vodafone.com>
2018-11-25 05:43:54 +1300

    fininshng services

--- src/services/PagesContentServices.js
+++ src/services/PagesContentServices.js
@@ -6,6 +6,20 @@ export const PagesContentServices = {
     },
     getMethodologyPageContent: function () {
         return Axios.get('https://api.jsonbin.io/b/5bf96221bd6fba76967f509a')
+    },
+    getStaffPageContent: function () {
+        return Axios.get('https://api.jsonbin.io/b/5bf97c9a82eab4765e8139d5')
+    },
+    getAdmissionPageContent: function () {
+        return Axios.get('https://api.jsonbin.io/b/5bf97d5a4f3bfe769ca34226')
+    },
+    getSafetyAndSecurityPageContent: function () {
+        return Axios.get('https://api.jsonbin.io/b/5bf97dcf484e637664002434')
+    },
+    getHealthPageContent: function () {
+        return Axios.get('https://api.jsonbin.io/b/5bf97e8a484e63766400247b')
+    },
+    getProgramsAndEventsPageContent: function () {
+        return Axios.get('https://api.jsonbin.io/b/5bf97f9582eab4765e813add')
     }
-
 }
