b254fc34fb0a021dcc23d31085433136c0c07b3f
Mohamed-AbdElMoniem-AbdElMegeed <mohamed.abdelmoniem-abdelmegeed@vodafone.com>
2018-11-27 08:40:41 +1300

    adding https

--- src/services/PagesContentServices.js
+++ src/services/PagesContentServices.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 export const PagesContentServices = {
     getHomeContent: function () {
-        return Axios.get('http://api.jsonbin.io/b/5bf81aaaaeadd5600f7989a9/2')
+        return Axios.get('https://api.jsonbin.io/b/5bf81aaaaeadd5600f7989a9/2')
     },
     getMethodologyPageContent: function () {
         return Axios.get('https://api.jsonbin.io/b/5bf96221bd6fba76967f509a')
